66ee9a3aecc1eefa79d26b582303c88bbc56fdbc
Mike North <michael.l.north@gmail.com>
2018-07-29 21:34:50 -0700

    fix: test

--- test/mike-north-types-test.ts
+++ test/mike-north-types-test.ts
@@ -3,5 +3,5 @@ import * as MNT from '@mike-north/types';
 QUnit.module('mike-north-types tests');
 
 QUnit.test('hello', assert => {
-  assert.equal(MNT, 'Hello from mike-north-types');
+  assert.ok(MNT);
 });
