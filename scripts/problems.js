f314efeec808abdd39ff32ef9c0f14a207be1ca6
Mike North <michael.l.north@gmail.com>
2018-12-04 14:26:00 -0800

    chore: replace travis-deploy-once with travis-ci stage
    Fixes #24

--- scripts/problems.js
+++ scripts/problems.js
@@ -1,9 +1,15 @@
 const shell = require('shelljs');
 const path = require('path');
 
-const formatters = path.join(path.dirname(require.resolve('libkit')), 'lib', 'vscode');
+const formatters = path.join(
+  path.dirname(require.resolve('libkit')),
+  'lib',
+  'vscode'
+);
 
 let tsc = shell.exec('tsc -p tsconfig.json --noEmit');
-let tslint = shell.exec(`tslint -p . --formatters-dir ${formatters} --format tsc`);
+let tslint = shell.exec(
+  `./node_modules/.bin/tslint -p . --formatters-dir ${formatters} --format tsc`
+);
 
 shell.exit(tsc.code || tslint.code);
